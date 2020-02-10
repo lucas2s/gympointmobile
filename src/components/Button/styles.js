5b1734d3cdfd3e09a44168a2d400e5f729b4b62e
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-02-10 00:44:11 -0300

    Criação component Button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
-import { BaseButton } from 'react-native-gesture-handler';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled(BaseButton)`
+export const Container = styled(RectButton)`
   padding: 0 15px;
   height: 45px;
   width: 325px;
