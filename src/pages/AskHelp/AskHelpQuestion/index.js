bc1b96f19edc107a93acea9cdd13f3db610cc0f9
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-03-02 01:15:30 -0300

    AskHelpQuestion criado

--- src/pages/AskHelp/AskHelpQuestion/index.js
+++ src/pages/AskHelp/AskHelpQuestion/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { Alert } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, Form, TextQuestion, ButtonSend } from './styles';
 
-export default function AskHelpQuestion() {
-  return <View />;
+import api from '~/services/api';
+
+export default function AskHelpQuestion({ navigation }) {
+  const [question, setQuestion] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const student = useSelector(state => state.auth.student);
+  const { id } = student;
+
+  async function handleAddQuestion() {
+    try {
+      setLoading(true);
+      const response = await api.post(`/students/${id}/help-orders`, {
+        question,
+      });
+
+      if (response.status !== 200) {
+        Alert.alert('Erro pedido de ajuda', response.data);
+      } else {
+        Alert.alert('Pedido de ajuda', 'Pedido de ajuda enviado com sucesso');
+        setLoading(false);
+        navigation.navigate('AskHelp');
+      }
+    } catch (err) {
+      setLoading(false);
+      Alert.alert(
+        'Erro pedido de ajuda',
+        'Não foi possível realizar o pedido de ajuda'
+      );
+    }
+  }
+
+  return (
+    <Container>
+      <Form>
+        <TextQuestion
+          keyboardType="default"
+          placeholder="Inclua seu pedido de auxílio"
+          returnKeyType="send"
+          value={question}
+          onChangeText={setQuestion}
+          multiline
+          numberOfLines={15}
+          textAlignVertical="top"
+        />
+        <ButtonSend loading={loading} onPress={handleAddQuestion}>
+          Enviar pedido
+        </ButtonSend>
+      </Form>
+    </Container>
+  );
 }
+
+AskHelpQuestion.propTypes = {
+  navigation: PropTypes.oneOfType([PropTypes.object, PropTypes.func])
+    .isRequired,
+};
--- src/pages/AskHelp/AskHelpQuestion/styles.js
+++ src/pages/AskHelp/AskHelpQuestion/styles.js
@@ -1,3 +1,30 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+
+export const Container = styled.View`
+  display: flex;
+  margin: 20px;
+  padding-bottom: 5px;
+`;
+
+export const Form = styled.View`
+  display: flex;
+`;
+
+export const TextQuestion = styled.TextInput`
+  align-items: flex-start;
+  justify-content: flex-start;
+  background: #ffffff;
+  border: 1px solid #dddddd;
+  border-radius: 4px;
+  height: 300px;
+  font-size: 16px;
+  line-height: 14px;
+  color: #444444;
+`;
+
+export const ButtonSend = styled(Button)`
+  margin-top: 15px;
+`;
